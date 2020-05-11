4963d64354501d61ad3fad6322f995678d51d3a5
Dimitriy Ni <dimitriy.ni@outlook.com>
2020-05-11 23:13:16 +0200

    added helper macro file. changed arg behaviour for grant_rights macro

--- macros/hook_functions/on_run_end_grant_rights.sql
+++ macros/hook_functions/on_run_end_grant_rights.sql
@@ -1,36 +1,39 @@
 {%- macro grant_read(users, schemas_list=none, include_main_schema=true) -%}
 
-  {% do schemas_list.append(target.schema) if include_main_schema and schemas_list %}
+  {% set users = gemma_dbt_utils.arg_into_list(users) %}
 
-  {% for user in users %}
+  {% set schemas_list = gemma_dbt_utils.arg_into_list(schemas_list) %}
 
-    {% if schemas_list %}
+  {%- do schemas_list.append(target.schema) if include_main_schema and schemas_list -%}
 
-      {% for schema in schemas_list %}
+  {%- for user in users -%}
 
-          {% set schema = target.schema+'_'+schema if schema!=target.schema else schema %}
+    {%- if schemas_list -%}
+      {%- for schema in schemas_list %}
 
-          grant usage on schema {{ schema }} to {{ user }};
-          grant select on all tables in schema {{ schema }} to {{ user }};
-
-      {% endfor %}
-
-    {% else %}
-
-      {% for schema in schemas %}
-
-        {% if include_main_schema or
-              (include_main_schema == false and schema != target.schema) %}
+          {%- set schema = target.schema+'_'+schema if schema!=target.schema else schema %}
 
           grant usage on schema {{ schema }} to {{ user }};
           grant select on all tables in schema {{ schema }} to {{ user }};
 
-        {% endif %}
+      {%- endfor %}
 
-      {% endfor %}
+    {%- else -%}
 
-    {% endif %}
+      {%- for schema in schemas -%}
 
-  {% endfor %}
+        {%- if include_main_schema or
+              (include_main_schema == false and schema != target.schema) -%}
+
+          grant usage on schema {{ schema }} to {{ user }};
+          grant select on all tables in schema {{ schema }} to {{ user }};
+
+        {%- endif -%}
+
+      {%- endfor %}
+
+    {%- endif -%}
+
+  {%- endfor -%}
 
 {%- endmacro -%}
