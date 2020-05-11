b7f29094156076ebbed038aad665e054617634b6
Dimitriy Ni <dimitriy.ni@outlook.com>
2020-05-11 17:22:31 +0200

    reduced code

--- macros/hook_functions/post_hook.sql
+++ macros/hook_functions/post_hook.sql
@@ -1,8 +1,6 @@
 {%- macro post_hook_index(target_column, unique=none, method=none) -%}
 
-  {%- set unique = 'unique' if unique == 'unique' or unique == true else '' -%}
-
-  create {{ unique }} index if not exists
+  create {{ 'unique' if unique else '' }} index if not exists
   {{ this.table }}__index_on_{{ target_column }} on {{ this }}
 
   {%- if method in ('btree','brin','hash','spgist') -%}
