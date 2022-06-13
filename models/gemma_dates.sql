a7290ddadf19c35c682345ab7f9d8bd125f34b08
msenyonyi <matthewsenyonyi@gmail.com>
2022-06-13 15:32:19 +0200

    Add year_quarter field to gemma_dates

--- models/gemma_dates.sql
+++ models/gemma_dates.sql
@@ -219,6 +219,7 @@
         AS next_month_first_day
       , DATE(DATE_TRUNC('month', date) + INTERVAL '2 month' - INTERVAL '1 day')
         AS next_month_last_day
+      , CONCAT(year, '-Q', quarter_num) AS year_quarter
 
     FROM dates
 
